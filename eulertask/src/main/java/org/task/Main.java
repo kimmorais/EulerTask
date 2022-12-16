abefcfda3fef52341fcc57e2a0ca74b986be8161
Kim Morais <kim_morais101@outlook.com>
2022-12-15 23:16:44 -0300

    testing parser class

--- eulertask/src/main/java/org/task/Main.java
+++ eulertask/src/main/java/org/task/Main.java
@@ -1,13 +1,18 @@
 package org.task;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        try (var inputStream = Main.class.getResourceAsStream("poker.txt")) {
+        var file = new File("C:\\poker.txt");
+
+        try (var inputStream = new FileInputStream(file)) {
 
             var parser = new Parser(inputStream);
+            var hands = parser.getPlayerHands();
         }
     }
 }
