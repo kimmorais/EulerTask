66c1df1edd1538c37650cdc5ced43441b36fa29c
Kim Morais <kim_morais101@outlook.com>
2022-12-21 05:58:03 -0300

    fixing rule

--- eulertask/src/main/java/org/task/solution/Decider.java
+++ eulertask/src/main/java/org/task/solution/Decider.java
@@ -66,7 +66,7 @@ public class Decider {
             var pOneCards = pOneHand.getCards();
             var pTwoCards = pTwoHand.getCards();
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 4; i >= 0; i--) {
 
                 var pOneHighestCardValue = pOneCards.get(i).getValue().getValueOrdinal();
                 var pTwoHighestCardValue = pTwoCards.get(i).getValue().getValueOrdinal();
